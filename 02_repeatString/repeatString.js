2364d7964df47b5774220f130cfc141b8d1cd2ca
Wong Yhung <wongyhung@gmail.com>
2025-03-22 21:51:07 +0800

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) { return "ERROR"
+    }   else {
+        let string = "";
+        for (let i = 0; i < num; i++) {
+            string += word;
+        }
+        return string;
+    }
 };
+
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
