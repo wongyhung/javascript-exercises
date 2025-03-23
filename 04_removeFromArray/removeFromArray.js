6c2a3046ef0317cd679e5a765780121c11eb2c9f
Wong Yhung <wongyhung@gmail.com>
2025-03-23 17:56:19 +0800

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(enteredArray, ...toBeRemoved) {
+    const newArray = [];
+
+    enteredArray.forEach((item) => {
+        if (!toBeRemoved.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
