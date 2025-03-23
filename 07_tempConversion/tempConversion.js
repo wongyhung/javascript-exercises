8ee49cb828a275a2e1cd50dea4e9fe012c203bd2
Wong Yhung <wongyhung@gmail.com>
2025-03-23 23:23:04 +0800

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * (5/9);
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * (9 / 5) + 32);
+    return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +13,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+ 
+
+// x °C ≘ (x * (⁠9/5) + 32) °F
+// x °F ≘ (x − 32) * ⁠(5/9) °C
