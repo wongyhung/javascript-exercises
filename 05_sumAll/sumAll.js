581a5d4b80e78717f5f9c85ef8344293aa2ca3f0
Wong Yhung <wongyhung@gmail.com>
2025-03-23 18:25:04 +0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let summedNum = 0;
+    if (typeof firstNum === "number" && typeof lastNum === "number") {
+
+        if (firstNum >= 0 && lastNum >= 0) {
+
+            if (Number.isInteger(firstNum) && Number.isInteger(lastNum)) {
+
+            if (firstNum < lastNum) {
+
+                for (let i = firstNum; firstNum <= lastNum; firstNum++) {
+                    summedNum += firstNum;
+                }
+
+            } else if (firstNum > lastNum) {
+
+                for (let i = lastNum; lastNum <= firstNum; lastNum++) {
+                    summedNum += lastNum;
+                }
+
+            }
+            } else return "ERROR";
+
+        } else return "ERROR";
+
+            return summedNum;
+
+    } else return "ERROR";
+
 
 };
 
