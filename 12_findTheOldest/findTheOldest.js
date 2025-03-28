dac6a65250f3b9be440a3717de664d050bf7421c
Wong Yhung <wongyhung@gmail.com>
2025-03-28 16:02:45 +0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear(); // Get current year
+        const age = (person.yearOfDeath || currentYear) - person.yearOfBirth; // Handle people still alive
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        
+        return age > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
