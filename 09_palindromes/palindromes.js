3d151ae73d7f98ba0ae4eb022778a99f513f3d09
Wong Yhung <wongyhung@gmail.com>
2025-03-28 15:53:45 +0800

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // Check if the cleaned string is equal to its reverse
+    return cleanedStr === cleanedStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
