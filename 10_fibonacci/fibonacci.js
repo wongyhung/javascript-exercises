85409314b65d0da18e940701964a8895a0c21006
Wong Yhung <wongyhung@gmail.com>
2025-03-28 15:56:42 +0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n); // Convert input to a number in case it's a string
 
+    if (n < 0) return "OOPS"; // Reject negative numbers
+    if (n === 0) return 0; // 0th Fibonacci number is 0
+    if (n === 1) return 1; // 1st Fibonacci number is 1
+
+    let a = 1, b = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
